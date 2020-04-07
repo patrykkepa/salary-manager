22ab724a128bad8b301ea1efd33dcddeb579d37b
Patryk <patryk@Mac-mini-Patryk.local>
2020-04-07 15:09:40 +0200

    Added basic folders structure

--- src/Routing.js
+++ src/Routing.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import classes from './App.module.scss';
 
-function App() {
+import classes from './Routing.module.scss';
+
+function Routing() {
   return (
     <div className={classes.App}>
       <header className={classes.Appheader}>
-        <img src={logo} className={classes.Applogo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className={classes.Applink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Dalej
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default Routing;
