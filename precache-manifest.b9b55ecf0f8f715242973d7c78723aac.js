3c2fae833b20c46607187386a4542b326c267a21
Patryk <patryk@Mac-mini-Patryk.local>
2020-04-07 13:57:06 +0200

    Updates

---
+++ precache-manifest.b9b55ecf0f8f715242973d7c78723aac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dfc542c48d7f87fd68e509d69f59b7d",
+    "url": "/salary-manager/index.html"
+  },
+  {
+    "revision": "d4d1a6e62a9f42bff127",
+    "url": "/salary-manager/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cedbf928e6c3558a6852",
+    "url": "/salary-manager/static/js/2.1c2eea40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/salary-manager/static/js/2.1c2eea40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4d1a6e62a9f42bff127",
+    "url": "/salary-manager/static/js/main.5c30f8e8.chunk.js"
+  },
+  {
+    "revision": "15d0b61a471eb71a3b21",
+    "url": "/salary-manager/static/js/runtime-main.b414d3ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/salary-manager/static/media/logo.5d5d9eef.svg"
+  }
+]);
